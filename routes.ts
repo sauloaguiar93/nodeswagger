ea612c87b62e1cb6e43570aa5f1c227f7d43cb3a
Saulo Aguiar <sauloaguiar93@hotmail.com>
2025-02-10 09:47:06 -0300

    API autodescritiva
    Cria uma API autodescritiva usando Fastify, Swagger, Node.js e a biblioteca Zod para validação e serialização de entradas. (Projeto RocketSeat)

---
+++ routes.ts
@@ -0,0 +1,51 @@
+import z from "zod";
+import { FastifyTypedInstance } from "./types";
+import { randomUUID } from "node:crypto";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const users = [];
+export async function routes(app: FastifyTypedInstance) {
+  app.get('/users', {
+    schema: {
+      tags: ['users'],
+      description: 'List user',
+      response: {
+        200: z.array(z.object({
+          id: z.string(),
+          name: z.string(),
+          email: z.string(),
+        })),
+      }
+    },
+  }, () => {
+    return []
+  });
+
+  app.post('/users', {
+    schema: {
+      tags: ['users'],
+      description: 'Create a new user',
+      body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+      }),
+      response: {
+        201: z.null().describe('User Created'),
+      },
+    }
+  }, async (request,reply) => {
+    const { name, email } = request.body;
+    users.push({
+      id: randomUUID(),
+      name,
+      email,
+    });
+
+    return reply.status(201).send();
+  });
+}
